e2791cd79d6d6b54a8d3cd46c7af6fe13e418bb4
Brad <brad.schwagler@gmail.com>
2014-12-09 16:31:17 -0800

    Added singleton class for settings

---
+++ src/com/bschwagler/positivity/GlobalsAreBad.java
@@ -0,0 +1,19 @@
+package com.bschwagler.positivity;
+
+//TODO Public settings. 
+//There's got to be a better way to do global variables? 
+
+public class GlobalsAreBad {
+    private static GlobalsAreBad mInstance= null;
+
+    public boolean vibEnabled;
+
+    protected GlobalsAreBad(){}
+
+    public static synchronized GlobalsAreBad getInstance(){
+    	if(null == mInstance){
+    		mInstance = new GlobalsAreBad();
+    	}
+    	return mInstance;
+    }
+}
